b2fca766568d963e7a72660d73e190c9a9fcd77c
최재용 <jdragon@i-screamedu.co.kr>
2022-01-07 16:12:47 +0900

    Initial commit

---
+++ src/test/java/com/exampls/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampls.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
