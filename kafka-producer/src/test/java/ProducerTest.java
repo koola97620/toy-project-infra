bacd56d15119245ffb8455c0a019e8ffb1111494
최재용 <jdragon@i-screamedu.co.kr>
2022-01-26 13:37:17 +0900

    test: 레디스템플릿 메서드 테스트

---
+++ kafka-producer/src/test/java/ProducerTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class ProducerTest {
+    @Test
+    void test() {
+        System.out.println("test");
+    }
+}
--- redis/src/test/java/com/redis/EmbeddedRedisConfig.java
+++ redis/src/test/java/com/redis/EmbeddedRedisConfig.java
@@ -1,39 +1,39 @@
-package com.redis;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import redis.embedded.RedisServer;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
-@Profile("h2")
-@Configuration
-public class EmbeddedRedisConfig {
-
-    private RedisServer redisServer;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
-
-    @PostConstruct
-    public void redisServer() {
-        if (redisServer == null || !redisServer.isActive()) {
-            System.out.println("Redis Start");
-            redisServer = RedisServer.builder()
-                    .port(redisPort)
-                    .setting("maxmemory 128M")  // 윈도우에서만 설정 필요
-                    .build();
-        }
-        redisServer.start();
-    }
-
-    @PreDestroy
-    public void stopRedis() {
-        if (redisServer != null) {
-            redisServer.stop();
-        }
-    }
-
-}
+//package com.redis;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import redis.embedded.RedisServer;
+//
+//import javax.annotation.PostConstruct;
+//import javax.annotation.PreDestroy;
+//
+//@Profile("h2")
+//@Configuration
+//public class EmbeddedRedisConfig {
+//
+//    private RedisServer redisServer;
+//
+//    @Value("${spring.redis.port}")
+//    private int redisPort;
+//
+//    @PostConstruct
+//    public void redisServer() {
+//        if (redisServer == null || !redisServer.isActive()) {
+//            System.out.println("Redis Start");
+//            redisServer = RedisServer.builder()
+//                    .port(redisPort)
+//                    .setting("maxmemory 128M")  // 윈도우에서만 설정 필요
+//                    .build();
+//        }
+//        redisServer.start();
+//    }
+//
+//    @PreDestroy
+//    public void stopRedis() {
+//        if (redisServer != null) {
+//            redisServer.stop();
+//        }
+//    }
+//
+//}
